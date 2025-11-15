36876d569b06a3964c0952767301c18545d5dfeb
AlbaVazquez <xavazgui498@ieshnosmachado.org>
2025-11-15 14:17:18 +0100

    .

--- Maquetas/Práctica cine/scripts.js
+++ Maquetas/Práctica cine/scripts.js
@@ -34,3 +34,22 @@ botonNext.addEventListener('click', () => {
 botonPrev.addEventListener('click', () => {
     moverCarrusel(-1);
 })
+
+//-----COMPORTAMIENTO DEL FRAME DEL MAPA
+
+document.addEventListener('DOMContentLoaded', function() {
+    const dondeEstamosLink = document.getElementById('donde-estamos-link');
+    const mapFrameContainer = document.getElementById('map-frame-container');
+    const closeMapFrame = document.getElementById('close-map-frame');
+
+    dondeEstamosLink.addEventListener('click', function(e) {
+        e.preventDefault();
+        mapFrameContainer.classList.add('show');
+        mapFrameContainer.classList.remove('hidden');
+    });
+
+    closeMapFrame.addEventListener('click', function() {
+        mapFrameContainer.classList.remove('show');
+        mapFrameContainer.classList.add('hidden');
+    });
+});
