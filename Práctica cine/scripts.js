181fdbe63eef7bf06ad98ae132c27b33cd2f9a58
AlbaVazquez <xavazgui498@ieshnosmachado.org>
2025-11-24 14:49:44 +0100

    .

--- Práctica cine/scripts.js
+++ Práctica cine/scripts.js
@@ -1,39 +1,3 @@
-//-----COMPORTAMIENTO DEL CARRUSEL-----
-const carrusel = document.querySelector('.carrusel');
-const items = document.querySelectorAll('.item-carrusel');
-let currentIndex = 0;
-const totalItems = items.length;
-
-function actualizarCarrusel() {
-    const desplazamiento = -currentIndex * 100;
-    carrusel.style.transform = `translateX(${desplazamiento}%)`;
-}
-
-function moverCarrusel(direction) {
-    currentIndex += direction;
-
-    if (currentIndex < 0) {
-        currentIndex = items.length - 1;
-    } else if (currentIndex >= items.length) {
-        currentIndex = 0;
-    }
-
-    actualizarCarrusel()
-}
-
-actualizarCarrusel()
-
-//-----COMPORTAMIENTO DE LOS BOTONES DEL CARRUSEL-----
-const botonPrev = document.querySelector('.prev-button');
-const botonNext = document.querySelector('.next-button');
-
-botonNext.addEventListener('click', () => {
-    moverCarrusel(1);
-})
-
-botonPrev.addEventListener('click', () => {
-    moverCarrusel(-1);
-})
 
 //-----COMPORTAMIENTO DEL FRAME DEL MAPA
 
